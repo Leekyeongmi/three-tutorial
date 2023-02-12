e96091f54acde5d2be5d18717290df6f39d7c3ca
Leekyeongmi <starry3ones@gmail.com>
2023-02-12 16:25:10 +0900

    1:모듈 형식으로 js 파일 불러오기

---
+++ 01/start/module_basic/main.js
@@ -0,0 +1,11 @@
+// import { hello1, hello2 } from './hello.js';
+import * as hello from './hello.js';
+
+hello.hello1();
+hello.hello2();
+
+// const { hello1, hello2 } = hello;
+// hello1();
+// hello2()
+
+//* 메인 js 파일에서 import 해와서 여러가지 js 파일을 연결한다.
